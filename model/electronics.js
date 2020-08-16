5d8445d9a3b0c9d5cd31fe6334effff7db1b2a07
Kwok <kwokrenx@gmail.com>
2020-08-15 23:06:54 -0400

    changed electronic schema up

--- model/electronics.js
+++ model/electronics.js
@@ -1,12 +1,13 @@
 const {Schema, model} = require('mongoose')
 
 const electronicsSchema = new Schema ({
-    name: String,
-    image: String,
-    brand: String,
-    description: String,
-    price: Number,
-    rating: [{type: Schema.Types.ObjectId, ref: "review"}]
+    Name: String,
+    Image: String,
+    Brand: String,
+    Description: String,
+    Price: Number,
+    Rating: Number,
+    Review: [{type: Schema.Types.ObjectId, ref: "review"}]
 })
 
 const electronics = model('electronic', electronicsSchema)
