39a2bd9c4cc516ed182b97b6b424cf1652de744e
Kristy Lee <leekristy01@gmail.com>
2020-08-15 23:57:18 -0400

    setting up files for controller

---
+++ controller/sellerElectronic.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const Electronic = require('../model/electronics')
+const Review = require('../model/review')
+
+// Click on Review button, which has an attribute id equal to the item ObjectId
+// Click on Submit of Review button, which will generate an ObjectId of that review
+
+// CREATE ELECTRONIC ITEM
+const create = async (req,res) => {
+    const review = Review.findOne({electronicItem: )}
+}
