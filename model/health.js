c524a5487e72d6943e3e9285157b78ba89ad8885
Kwok <kwokrenx@gmail.com>
2020-08-15 22:52:57 -0400

    finished health model

--- model/health.js
+++ model/health.js
@@ -1,1 +1,17 @@
 const {Schema, model} = require ('mongoose');
+
+const healthSchema = new Schema (
+    {
+        Name: String,
+        Image: String,
+        Brand: String,
+        Description: String,
+        Price: Number,
+        Rating: Number,
+        Review: [{type: Schema.Types.ObjectId, ref: "review"}]
+    }
+)
+
+const health = model('health', healthSchema);
+
+module.exports = health;
