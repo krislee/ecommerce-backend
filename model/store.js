2eeaf18472973d6d2ccfe8d0e0ded2162f3a028c
Kwok <kwokrenx@gmail.com>
2020-08-15 22:55:50 -0400

    finished store model

--- model/store.js
+++ model/store.js
@@ -1,1 +1,12 @@
 const {Schema, model} = require ('mongoose');
+
+const storeSchema = new Schema (
+    {
+        Electronics: [{type: Schema.Types.ObjectId, refer: "electronics"}],
+        Health: [{type: Schema.Types.ObjectId, refer: "health"}],
+        Clothing: [{type: Schema.Types.ObjectId, refer: "clothing"}],
+    }
+)
+
+const store = model('store', storeSchema);
+module.exports = store
