29b867804cd965c65cd88916ab34d1c7e622acfc
Kristy Lee <leekristy01@gmail.com>
2020-10-10 18:52:03 -0400

    middleware notes

--- server.js
+++ server.js
@@ -41,7 +41,10 @@ const corsOptions = {
   },
 };
 
-// MIDDLEWARE
+// MIDDLEWARE: functions that will run during routes hits. Middlewares can be route-specific, running only for that route (but that would be really route handlers), or in app.use(), running whenever any routes are hit. The order of middleware run is the order you write your middleware since next() in middlewares(i.e. function middleware(req, res, next){...next()}) will run the next middleware written. If you put in errObj = new Error('I am an error') inside next(errObj) to catch any errors, then when that regular middleware function runs and next(errObj) is inside the middleware function the 'I am an error' with call stack is displayed when any routes are hit. This will crash the server, so instead of using new Error object, we want to have an err param in the reg middleware function to catch any errors during when the server runs, such as when making calls to the db: function errorHandler(err, req, res, next){if(err){res.send('There was an error')}}. Put app.use(errorHandler) after all the middleware functions and after all the routes since if there is any error in the middleware or routes, then the error goes straight to the errorHandler middleware function skipping any other middlewares or routes before the errorHandler middleware function. Express passes the req and res objects to each middlewares.
+
+// Passport is an express middleware that will append diff properties to the req object, so you can store data within the req obj and each of the middlewares after will have access to the modified req object
+
 // Put ternary to see if sites are allowed before making the server run in app.use()
 NODE_ENV === "development" ? app.use(cors()) : app.use(cors(corsOptions)); // If in development, allow all websites; if in production, allow websites in whitelist to make API calls to server
 app.use(express.json()); // Turns JSON from post/put/patch requests and converts them into req.body object
