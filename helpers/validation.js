f1655d467758d625918417cbd07ca7fa2f87b9df
Kwok <kwokrenx@gmail.com>
2020-10-10 15:00:12 -0400

    added validation

---
+++ helpers/validation.js
@@ -0,0 +1,11 @@
+const Joi = require('@hapi/joi');
+
+const authSchema = Joi.object({
+    username: Joi.string().min(8).max(255).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(8).max(255).required()
+})
+
+module.exports = {
+    authSchema
+}
