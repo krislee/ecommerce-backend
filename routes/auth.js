3c25810e3e8d9e4acb649bc551205f071df1c197
Kwok <kwokrenx@gmail.com>
2020-10-12 19:59:14 -0400

    add hash and salt

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const User = require('../model/user')
 const { authSchema } = require('../helpers/validation')
+const bcrypt = require('bcrypt');
 
 router.post('/register', async (req, res) => {
     try {
-        const { username, password, email } = req.body;
+        // const { username, password, email } = req.body;
         const result = await authSchema.validateAsync(req.body);
         console.log(result, "result")
         const doesExistEmail = await User.findOne({email: result.email})
@@ -17,8 +18,10 @@ router.post('/register', async (req, res) => {
             res.status(400).send(`${result.username} is already taken. Please try a different one.`)
             return
         }
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+
         console.log(doesExistEmail, "doesexistemail")
-        const user = await User.create(req.body)
+        const user = await User.create({ username: req.body.username, password: hashedPassword, email: req.body.email})
         console.log(user, "user")
         const savedUser = await user.save();
         res.status(200).json(savedUser)
@@ -27,6 +30,22 @@ router.post('/register', async (req, res) => {
     }
 });
 
+router.post('/login', async (req, res) => {
+    const existUser = await User.findOne({username : req.body.username})
 
+    if (!existUser){
+        return res.status(400).send('Cannot find user')
+    }
+
+    try {
+        if (await bcrypt.compare(req.body.password, existUser.password)){
+            res.status(200).send('Success')
+        } else {
+            res.status(400).send('Not allowed')
+        }
+    } catch (err) {
+        res.status(400).send(err)
+    }
+})
 
 module.exports = router;
