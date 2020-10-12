b394e096dedc092675d4001099957bfca7dded1e
Kwok <kwokrenx@gmail.com>
2020-10-12 19:26:19 -0400

    jwt test

---
+++ helpers/auth.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+const jwt = require('jsonwebtoken')
+require("dotenv").config()
+
+app.use(express.json())
+
+let refreshTokens = []
+
+app.post('/login', (req, res) => {
+    // Authenticate User
+    // Grabs the username
+    const username = req.body.username
+    const user = { name: username }
+    // sign is a method takes as arguments the payload to serialize (which is our user object) and the secret key (which we make up)
+    const accessToken = generateAccessToken(user)
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN)
+    refreshTokens.push(refreshToken)
+    res.json({ accessToken: accessToken, refreshToken: refreshToken})
+})
+
+app.post('/token', (req, res) => {
+    const refreshToken = req.body.token
+    if (refreshToken == null) return res.sendStatus(401)
+    if (refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN, (err, user) => {
+        if (err) return res.sendStatus(403)
+        const accessToken = generateAccessToken({name: user.name})
+    res.json({ accessToken: accessToken})
+    })
+})
+
+app.delete('/logout', (req, res) => {
+    refreshTokens = refreshTokens.filter(token => token !== req.body.token)
+    res.sendStatus(204)
+})
+
+function generateAccessToken(user) {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+}
+
+app.listen(4000, () => {
+    console.log("This is running")
+})
