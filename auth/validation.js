47fa33242194a3914b9ec6da7685a4ff47eabb9e
Kristy Lee <leekristy01@gmail.com>
2020-10-21 16:04:10 -0400

    removed seller from Joi

--- auth/validation.js
+++ auth/validation.js
@@ -4,7 +4,6 @@ const authSchema = Joi.object({
     username: Joi.string().min(8).max(255).required(),
     email: Joi.string().email().required(),
     password: Joi.string().min(8).max(255).required(),
-    seller: Joi.boolean().required()
 })
 
 module.exports = {
