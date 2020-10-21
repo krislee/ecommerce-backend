28fd343eee3a018d3feb34274afb459afcb9e435
Kristy Lee <leekristy01@gmail.com>
2020-10-21 11:21:38 -0400

    changed function for reviews in seller

--- routes/seller/electronic.js
+++ routes/seller/electronic.js
@@ -10,7 +10,7 @@ router.get('/electronic', passportAuthenticate, index)
 
 router.get('/electronic/:id', passportAuthenticate, show)
 
-router.get('/electronic/reviews/:id', passportAuthenticate, show)
+router.get('/electronic/reviews/:id', passportAuthenticate, indexReviews)
 
 router.post('/electronic', passportAuthenticate, create)
 
