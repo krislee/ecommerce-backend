97205efd8f4fca9fb8603e64bdbe169d972e22bb
Kwok <kwokrenx@gmail.com>
2020-10-26 10:07:29 -0400

    changed user to buyeruser

--- routes/buyer/buyerAuth.js
+++ routes/buyer/buyerAuth.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const User = require('../../model/buyer/buyerUser')
+const {BuyerUser} = require('../../model/buyer/buyerUser')
 const {SellerUser} = require('../../model/seller/sellerUser')
 const { authSchema } = require('../../auth/validation')
 const bcrypt = require('bcrypt');
@@ -10,9 +10,8 @@ router.post('/register', async (req, res) => {
     try {
         // const { username, password, email } = req.body;
         const result = await authSchema.validateAsync(req.body);
-
         // Check if email user is trying to register acct with is already in the db
-        const doesExistEmail = await User.findOne({email: result.email})
+        const doesExistEmail = await BuyerUser.findOne({email: result.email})
         const doesExistSellerEmail = await SellerUser.findOne({email: result.email})
         if (doesExistEmail || doesExistSellerEmail) {
             res.status(400).json({success: false, msg: `${result.email} is already registered.`})
@@ -20,7 +19,7 @@ router.post('/register', async (req, res) => {
         }
 
         // Check if username is trying to register acct with is already in the db
-        const doesExistUser = await User.findOne({ username: result.username})
+        const doesExistUser = await BuyerUser.findOne({ username: result.username})
         const doesExistSellerUser = await SellerUser.findOne({username: result.username})
         if (doesExistUser || doesExistSellerUser) {
             res.status(400).json({success: false, msg:`${result.username} is already taken. Please try a different one.`})
@@ -33,7 +32,7 @@ router.post('/register', async (req, res) => {
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
 
         // 2) Create and save the created user to db
-        const user = await User.create({ username: req.body.username, password: hashedPassword, oldPasswords: hashedPassword, email: req.body.email, buyer: true})
+        const user = await BuyerUser.create({ username: req.body.username, password: hashedPassword, oldPasswords: hashedPassword, email: req.body.email, buyer: true})
         const savedUser = await user.save();
 
         // 3) Create JWT token for successfully registered user
@@ -54,7 +53,7 @@ router.post('/register', async (req, res) => {
 router.post('/login', async (req, res) => {
 
     // Find user by unique username
-    const existUser = await User.findOne({username : req.body.username})
+    const existUser = await BuyerUser.findOne({username : req.body.username})
 
     if (!existUser){
         return res.status(400).json({success: false, msg: 'Cannot find user'})
