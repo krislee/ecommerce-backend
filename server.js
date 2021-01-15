a085557b0bdb7eaca41623575fe92f9b0433f6f6
Kristy Lee <leekristy01@gmail.com>
2021-01-15 18:06:52 -0500

    changed url

--- server.js
+++ server.js
@@ -123,7 +123,7 @@ app.use(function(req, res, next) {
   // console.log("res:", res)
   res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-  res.setHeader("Access-Control-Allow-Origin", "https://elecommerce.netlify.app, https://heuristic-heyrovsky-931d8c.netlify.app");
+  res.setHeader("Access-Control-Allow-Origin", ["https://elecommerce.netlify.app, https://heuristic-heyrovsky-931d8c.netlify.app"]);
   // res.setHeader("Access-Control-Allow-Origin", "https://affectionate-nightingale-c0410a.netlify.app");
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Set-Cookie, Cookie, X-Forwarded-Proto");
   // console.log("res 2:", res)
