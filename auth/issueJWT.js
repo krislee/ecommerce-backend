c56aa235acddf2bb528dc2c68391274f71ef0309
Kristy Lee <leekristy01@gmail.com>
2021-01-24 15:12:15 -0500

    trying to resolve auth login error

--- auth/issueJWT.js
+++ auth/issueJWT.js
@@ -22,8 +22,6 @@ function issueJWT(user){
     // Token will expire in 60days
     const expiresIn = '60d'
     
-    console.log(25, JSON.parse(process.env.SESSION_SECRET))
-    console.log(24, process.env.SESSION_SECRET)
     // console.log(26, JSON.parse(process.env.SESSION_SECRET).replace(/\\n/g, '\n'))
 
     
@@ -31,7 +29,7 @@ function issueJWT(user){
     ////// CREATE TOKEN //////
 
     // Using the algo, hash the data that contains the header and payload & then sign that data with the private key (header is auto created by the jsonwebtoken library). By doing the hashing and signing of the data, the token now has the signature (signature = hashed header + hashed payload + private key) of the JWT. 
-    const signedToken = jsonwebtoken.sign(payload, JSON.parse(process.env.PRIV_KEY), {expiresIn: expiresIn, algorithm: 'RS256'})
+    const signedToken = jsonwebtoken.sign(payload, process.env.PRIV_KEY.replace(/\n/g, '\n'), {expiresIn: expiresIn, algorithm: 'RS256'})
     
     return {
         token: "Bearer " + signedToken,
