53d63f74fa82cb00cb117d9f4c5b069357e4fba6
Kristy Lee <leekristy01@gmail.com>
2021-01-24 14:59:27 -0500

    testing auth

--- auth/issueJWT.js
+++ auth/issueJWT.js
@@ -1,7 +1,7 @@
+require("dotenv").config()
 const jsonwebtoken = require('jsonwebtoken');
 const fs = require('fs');
 const path = require('path');
-require("dotenv").config()
 
 // Path to Private Key File
 // const pathToKey = path.join(__dirname, '.', 'id_rsa_priv.pem')
@@ -10,18 +10,24 @@ require("dotenv").config()
 
 function issueJWT(user){
     ////// CREATE PAYLOAD TO BE PART OF THE TOKEN //////
+    console.log(13)
     const payload = {
         sub: user._id, // user ID
         iat: Date.now(), // JWT issued at date
     }
-
+    console.log(18, "payload: ", payload)
 
     ////// EXPIRATION OF TOKEN //////
 
     // Token will expire in 60days
     const expiresIn = '60d'
+    
+    console.log(25, JSON.parse(process.env.SESSION_SECRET))
+    console.log(24, process.env.SESSION_SECRET)
+    // console.log(26, JSON.parse(process.env.SESSION_SECRET).replace(/\\n/g, '\n'))
 
-    console.log(24, "JSON.parse(process.env.PRIV_KEY): ", JSON.parse(process.env.PRIV_KEY))
+    
+    // console.log(25, "JSON.parse(process.env.PRIV_KEY): ", JSON.parse(process.env.PRIV_KEY))
     ////// CREATE TOKEN //////
 
     // Using the algo, hash the data that contains the header and payload & then sign that data with the private key (header is auto created by the jsonwebtoken library). By doing the hashing and signing of the data, the token now has the signature (signature = hashed header + hashed payload + private key) of the JWT. 
