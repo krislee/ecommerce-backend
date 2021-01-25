15db95c0b7b78002ee307be1e3d5613d747a2e87
Kristy Lee <leekristy01@gmail.com>
2021-01-25 13:04:25 -0500

    update item quantity in successful payment intent webhook

--- controller/buyer/stripeWebhook.js
+++ controller/buyer/stripeWebhook.js
@@ -3,6 +3,7 @@ const stripe = require("stripe")(`${process.env.STRIPE_SECRET}`)
 const { v4: uuidv4 } = require('uuid');
 const Cart = require('../../model/buyer/cart')
 const Order = require('../../model/orders')
+const Electronic = require('../../model/seller/electronic')
 
 // Each endpoint (the proj's endpoint is /webhook/events) listens to some events that you designate the event to listen to (designate in the Stripe Dashboard). Since Stripe optionally signs the event that is sent to the endpoint, where the signature value is stored in the Stripe-Signature header, you can check if Stripe was the one that sent the event and not some third party. Webook event signing happens by using the Stripe's library and providing the library the endpoint secret, event payload, and Stripe-Signature header.  
 
@@ -62,14 +63,21 @@ const webhook = async (req, res) => {
             }
             console.log("update recollect_cvv payment method after successful payment: ", paymentMethod)
 
-            // Fulfill order by retrieving the items from the Cart document before deleting the cart later. 
-            const order = Order.create({
+            // Fulfill order by retrieving the items from the Cart document before deleting the cart later. While retrieving the Cart items, update the Electronic item quantity.
+            const order = await Order.create({
                 LoggedInBuyer: data.object.customer,
                 OrderNumber: uuidv4() // generate random order ID number using uuid 
             })
-            const cart = Cart.findOne({LoggedInBuyer: data.object.customer})
+            const cart = await Cart.findOne({LoggedInBuyer: data.object.customer})
             for(let i=0; i < cart.Items.length; i++){
+
                 order.Items.push(cart.Items[[i]])
+
+                // Update inventory quantity of the items sold
+                const electronic = await Electronic.findById(cart.Items[i].ItemId)
+                electronic.Quantity -= cart.Items[i].Quantity
+
+                console.log("updated quantity electronic: ", electronic)
             }
             console.log("logged in order: ", order)
 
@@ -78,10 +86,17 @@ const webhook = async (req, res) => {
             console.log("logged in cart deleted: ", deletedCart)
             
         } else {
-            // Fulfill order by retrieving the items from the Cart document before deleting the cart later. 
+            // Fulfill order by retrieving the items from the Cart document before deleting the cart later. While retrieving the Cart items, update the Electronic item quantity.
             const order = Order.create({OrderNumber: uuidv4()})
             for(let i=0; i < req.session.cart.length; i++) {
+
                 order.Items.push(req.session.cart[i])
+
+                // Update inventory quantity of the items sold
+                const electronic = await Electronic.findOneAndUpdate(req.session.cart[i].ItemId)
+                electronic.Quantity -= cart.Items[i].Quantity
+
+                console.log("updated quantity electronic: ", electronic)
             }
             console.log("guest order: ", order)
 
@@ -90,14 +105,6 @@ const webhook = async (req, res) => {
             delete req.session.cart
             console.log("delete req.session after successful payment: ", req.session)
         }
-        
-        
-        // Update quantity of items
-        
-
-        // Email receipts
-
-
 
         //////////////////////////////////
         // Delete the saved idempotency associated with the payment intent in CachePaymentIntent for the guest(?) since the payment intent is successful???
