00ded9a1a048d06d67886da42b1d5f2faa59311a
Kristy Lee <leekristy01@gmail.com>
2021-02-12 22:42:05 -0500

    have to switch req and res as params of router callback to resolve TypeError: Cannot read property 'id' of undefined

--- routes/buyer/order.js
+++ routes/buyer/order.js
@@ -6,7 +6,7 @@ const Order = require('../../model/order')
 
 passportAuthenticate = passport.authenticate('jwt', {session: false})
 
-router.get('/order/:id', async(res, req) => {
+router.get('/order/:id', async(req, res) => {
     console.log(10)
     console.log(11, req.params.id)
     const order = Order.findOne({CartID: req.params.id})
