edcb4ee79196eeb967439e27df9de79788427d74
Kristy Lee <leekristy01@gmail.com>
2021-02-13 18:32:15 -0500

    revised order route

--- routes/buyer/order.js
+++ routes/buyer/order.js
@@ -8,29 +8,30 @@ const Order = require('../../model/order')
 passportAuthenticate = passport.authenticate('jwt', {session: false})
 
 router.get('/list/orders/:id', async(req, res) => {
-    const order = await Order.findById(req.params.id)
+    const order = await Order.findOne({OrderNumber: req.params.id})
     console.log(11, order)
     res.status(200).json({order: order})
 })
 
-router.get('/order/payment-intent/:id', async(req, res) => {
-    console.log(17, typeof req.params.id)
-    const succeededPaymentIntent = await stripe.paymentIntents.retrieve(req.params.id)
-    console.log(18, succeededPaymentIntent)
-    const paymentMethod = await stripe.paymentMethods.retrieve(succeededPaymentIntent.payment_method)
-    console.log(20, paymentMethod)
-    res.status(200).json({
-        orderNumber: succeededPaymentIntent.metadata.order_number,
-        cardBrand: paymentMethod.card.brand,
-        billingDetails: {
-            name: paymentMethod.billingDetails.name,
-            line1: paymentMethod.billingDetails.address.line1,
-            line2: paymentMethod.billingDetails.address.line2,
-            city: paymentMethod.billingDetails.address.city,
-            state: paymentMethod.billingDetails.address.state,
-            postalCode: paymentMethod.billingDetails.address.postal_code
-        }
-    })
-})
+// router.get('/order/payment-intent/:id', async(req, res) => {
+//     console.log(17, typeof req.params.id)
+//     const succeededPaymentIntent = await stripe.paymentIntents.retrieve(req.params.id)
+//     console.log(18, succeededPaymentIntent)
+//     const paymentMethod = await stripe.paymentMethods.retrieve(succeededPaymentIntent.payment_method)
+//     console.log(20, paymentMethod)
+//     console.log(22, paymentMethod.billingDetails)
+//     res.status(200).json({
+//         orderNumber: succeededPaymentIntent.metadata.order_number,
+//         cardBrand: paymentMethod.card.brand,
+//         billingDetails: {
+//             name: paymentMethod.billing_details.name,
+//             line1: paymentMethod.billing_details.address.line1,
+//             line2: paymentMethod.billing_details.address.line2,
+//             city: paymentMethod.billing_details.address.city,
+//             state: paymentMethod.billing_details.address.state,
+//             postalCode: paymentMethod.billing_details.address.postal_code
+//         }
+//     })
+// })
 
 module.exports = router
