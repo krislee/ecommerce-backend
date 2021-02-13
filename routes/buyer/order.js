43dcfc29c81b618ed6e3de39de92e8bf9276107a
Kristy Lee <leekristy01@gmail.com>
2021-02-13 16:48:08 -0500

    debugging getting payment intents in order route

--- routes/buyer/order.js
+++ routes/buyer/order.js
@@ -7,13 +7,14 @@ const Order = require('../../model/order')
 
 passportAuthenticate = passport.authenticate('jwt', {session: false})
 
-router.get('/orders/:id', async(req, res) => {
+router.get('/list/orders/:id', async(req, res) => {
     const order = await Order.findById(req.params.id)
     console.log(11, order)
     res.status(200).json({order: order})
 })
 
 router.get('/order/payment-intent/:id', async(req, res) => {
+    console.log(17, typeof req.params.id)
     const succeededPaymentIntent = await stripe.paymentIntents.retrieve(req.params.id)
     console.log(18, succeededPaymentIntent)
     const paymentMethod = await stripe.paymentMethods.retrieve(succeededPaymentIntent.payment_method)
