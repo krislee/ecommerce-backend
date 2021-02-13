2c5c5171b74929850db5e330565b0af5b6a7c805
Kristy Lee <leekristy01@gmail.com>
2021-02-13 14:00:41 -0500

    debugging adding cart item; when adding more than one, it concatenates

--- controller/buyer/loggedInCart.js
+++ controller/buyer/loggedInCart.js
@@ -24,10 +24,13 @@ const loggedInAddItem = async(req, res, next) => {
                     console.log(req.params.id, typeof req.params.id, "req.params.id")
                     return i.ItemId === req.params.id
                 })
-                console.log(cartItem, "cartItem")
+                console.log(27, cartItem, "cartItem")
                 // if the item exists then update quantity and total price in the cart
                 if(cartItem) {
+                    console.log(30, "cart quantity: ", cartItem.Quantity, typeof cartItem.Quantity)
+                    console.log(31, "req quantity: ",  req.body.Quantity, typeof req.body.Quantity)
                     cartItem.Quantity += req.body.Quantity
+                    console.log(cartItem.Quantity, typeof cartItem.Quantity)
                     cartItem.TotalPrice = (item.Price * cartItem.Quantity) // get price from server and not from client side to ensure charge is not made up
                 } else { // if the item does not exist in the cart, then add the item
                     cart.Items.push({
