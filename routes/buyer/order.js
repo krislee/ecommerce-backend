e79af4b405ef8f3770164ff00af21c747521e418
Kristy Lee <leekristy01@gmail.com>
2021-02-12 22:50:56 -0500

    trying to debug this error with await:  UnhandledPromiseRejectionWarning: TypeError: Converting circular structure to JSON 2021-02-13T03:48:02.497244+00:00 app[web.1]: --> starting at object with constructor 'NativeTopology' 2021-02-13T03:48:02.497244+00:00 app[web.1]: |     property 's' -> object with constructor 'Object' 2021-02-13T03:48:02.497245+00:00 app[web.1]: |     property 'sessionPool' -> object with constructor 'ServerSessionPool'

--- routes/buyer/order.js
+++ routes/buyer/order.js
@@ -9,7 +9,8 @@ passportAuthenticate = passport.authenticate('jwt', {session: false})
 router.get('/order/:id', async(req, res) => {
     console.log(10)
     console.log(11, req.params.id)
-    const order = Order.findOne({CartID: req.params.id})
+    const order = await Order.findOne({CartID: req.params.id})
+    console.log(13, order)
     res.status(200).json({order: order})
 })
 
