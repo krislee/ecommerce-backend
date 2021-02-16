d61d3678bbdc710d4f275fe7e1347b2e162dffed
Kristy Lee <leekristy01@gmail.com>
2021-02-16 10:49:21 -0500

    revising create review controller to check if vuyer who wants to review actually brought the item

--- controller/buyer/electronicReview.js
+++ controller/buyer/electronicReview.js
@@ -1,7 +1,7 @@
 const {Electronic} = require('../../model/seller/electronic')
 const {ElectronicReview} = require('../../model/buyer/reviewElectronic')
 const Buyer = require('../../model/buyer/buyerUser')
-
+const Order = require('../../model/order')
 // SHOW ALL REVIEWS OF ONE ELECTRONIC ITEM
 
 const index = async (req, res) => {
@@ -37,8 +37,16 @@ const index = async (req, res) => {
 const create = async (req,res) => {
     try {
         // console.log(req.user)
-        if (req.user.buyer){
+        if (req.user.buyer){ // user needs to be logged in to review, hence req.user.buyer
+            // Check if logged in reviewer actually brought the electronic item before buyer can review
+            const purchasedOrders = await Order.find({LoggedInBuyer: req.user._id}).populate({
+                path: 'ElectronicItem', 
+                match: {_id: req.params.id}
+            })
+            console.log(43, purchasedOrders)
+            
             // From the frontend, the req.params will have the id of the electronic item. The item's id is grabbed when we click on the review button under each electronic item since each review button has an attribute id equal to the electronic item ObjectId
+            // if(purchasedOrders)
             const electronicReview = await ElectronicReview.create({
                 Name: req.user.username,
                 Comment: req.body.Comment,
