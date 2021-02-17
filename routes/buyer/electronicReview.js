c3b6b70cbe06f6688b25e13811b755bea1977194
Kristy Lee <leekristy01@gmail.com>
2021-02-16 20:20:41 -0500

    remove passport to see if it is affecting the route that is giving a 400 error: stringValue: "review",     kind: ObjectId,     value: review,     path: _id,     reason: {}

--- routes/buyer/electronicReview.js
+++ routes/buyer/electronicReview.js
@@ -5,7 +5,7 @@ const {index, create, update, destroy} = require('../../controller/buyer/electro
 const router = express.Router();
 const passportAuthenticate = passport.authenticate('jwt', {session: false})
 
-router.get('/electronic/review', passportAuthenticate, index)
+router.get('/electronic/review', index)
 
 router.post('/electronic/review/:electronicId', passportAuthenticate, create)
 
@@ -15,4 +15,4 @@ router.delete('/electronic/review/:id', passportAuthenticate, destroy)
 
 
 
-module.exports = router
+module.exports = router 
