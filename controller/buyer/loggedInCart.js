153277afa76b468f6d7d374350c6863c6e39c9dd
Kristy Lee <leekristy01@gmail.com>
2021-02-17 15:58:25 -0500

    removing the select

--- controller/buyer/loggedInCart.js
+++ controller/buyer/loggedInCart.js
@@ -47,7 +47,9 @@ const loggedInAddItem = async(req, res, next) => {
                 }
 
                 await cart.save()
-                
+
+                const updatedCartWithItem = await Cart.findOne({LoggedInBuyer: req.user._id}).select({"Items.Quantity": 1, "_id": 0, "LoggedInBuyer": 0});
+                console.log(51, updatedCartWithItem)
                 return res.status(200).json(cart)
 
             } else { // if cart does not exist create a new cart to hold the added item 
