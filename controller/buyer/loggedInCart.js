41e775f6e0032776459a5b5caf68de460ec4aee5
Kristy Lee <leekristy01@gmail.com>
2021-02-17 15:34:15 -0500

    trying to select only the quantity in cart

--- controller/buyer/loggedInCart.js
+++ controller/buyer/loggedInCart.js
@@ -48,7 +48,7 @@ const loggedInAddItem = async(req, res, next) => {
 
                 await cart.save()
 
-                const updatedCartWithItem = await Cart.find({LoggedInBuyer: req.buyer._id}).select( "Items.Quantity");
+                const updatedCartWithItem = await Cart.find({"Items.Quantity": 1});
                 console.log(51, updatedCartWithItem)
                 return res.status(200).json(cart)
 
