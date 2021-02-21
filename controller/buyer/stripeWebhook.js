dc75c9ec9a4d1948e89789a830654e07ed4c847a
Kristy Lee <leekristy01@gmail.com>
2021-02-21 17:17:48 -0500

    comment out socket in webhook; still trying to see if id can be sent

--- controller/buyer/stripeWebhook.js
+++ controller/buyer/stripeWebhook.js
@@ -166,26 +166,26 @@ const webhook = async (req, res) => {
 
                 // Send back order to client via websocket 
                 const io = req.app.get('socketio')
-                io.on('connection', (socket) => {
-                    console.log(171)
-                    const socketId = socket.id
-                    console.log(173, socketId)
-                    io.to(socketId).emit("completeOrder", {order: updateOrderWithShippingAndPayment, payment: {
-                        brand: paymentMethod.card.brand,
-                        last4: paymentMethod.card.last4,
-                        billingDetails: {
-                            address: {
-                                line1: paymentMethod.billing_details.address.line1,
-                                line2: paymentMethod.billing_details.address.line2,
-                                city:  paymentMethod.billing_details.address.city,
-                                state:  paymentMethod.billing_details.address.state,
-                                postalCode:  paymentMethod.billing_details.address.postal_code,
-                                country:  paymentMethod.billing_details.address.country
-                            },
-                            name: paymentMethod.billing_details.name
-                        }
-                    }})
-                })
+                // io.on('connection', (socket) => {
+                //     console.log(171)
+                //     const socketId = socket.id
+                //     console.log(173, socketId)
+                //     io.to(socketId).emit("completeOrder", {order: updateOrderWithShippingAndPayment, payment: {
+                //         brand: paymentMethod.card.brand,
+                //         last4: paymentMethod.card.last4,
+                //         billingDetails: {
+                //             address: {
+                //                 line1: paymentMethod.billing_details.address.line1,
+                //                 line2: paymentMethod.billing_details.address.line2,
+                //                 city:  paymentMethod.billing_details.address.city,
+                //                 state:  paymentMethod.billing_details.address.state,
+                //                 postalCode:  paymentMethod.billing_details.address.postal_code,
+                //                 country:  paymentMethod.billing_details.address.country
+                //             },
+                //             name: paymentMethod.billing_details.name
+                //         }
+                //     }})
+                // })
             } else {
                 // Fulfill order by retrieving the items from the Cart document before deleting the cart later. While retrieving the Cart items, update the Electronic item quantity.
                 try {
