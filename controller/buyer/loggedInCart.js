68a31da125ac65d2310f9444c48f8345c24be0f9
Kristy Lee <leekristy01@gmail.com>
2021-02-24 21:30:52 -0500

    destroy session after syncing cart

--- controller/buyer/loggedInCart.js
+++ controller/buyer/loggedInCart.js
@@ -170,7 +170,7 @@ const addItemsFromGuestToLoggedIn = async (req, res) => {
                 await newCart.save()
 
                 // then delete the cart from the session after adding all the items from cart
-                delete req.session
+                req.sessionStore.destroy(req.sessionID)
 
                 console.log(162, "after deleting session: ", req.session)
             }
