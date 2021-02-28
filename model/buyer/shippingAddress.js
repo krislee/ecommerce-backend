a31ffba8983a45f26a3f5380d69192326e3c1762
Kristy Lee <leekristy01@gmail.com>
2021-02-27 20:00:42 -0500

    added phone field to shippinh model

--- model/buyer/shippingAddress.js
+++ model/buyer/shippingAddress.js
@@ -3,6 +3,7 @@ const {Schema, model} = require('mongoose')
 const buyerShippingAddressSchema = new Schema({
     Name: {type: String, min: 8, max: 255, required: true},
     Address: {type: String, max: 255, required: true},
+    Phone: Number,
     DefaultAddress: {type: Boolean, required: true, default: false},
     LastUsed: {type: Boolean, required: true, default: false},
     Buyer: [{type: Schema.Types.ObjectId, ref: "BuyerUser"}]
