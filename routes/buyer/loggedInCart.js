a5ad9a3adb02e591a2412257dcaf33ae47340402
Kristy Lee <leekristy01@gmail.com>
2021-04-20 13:06:10 -0400

    corrected naming for getting cart item quantinty fo rlogged in user

--- routes/buyer/loggedInCart.js
+++ routes/buyer/loggedInCart.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const passport = require('passport');
 const passportAuthenticate = passport.authenticate('jwt', {session: false})
 
-const {loggedInAddItem, loggedInUpdateItemQuantity, loggedInDeleteItem, loggedInIndexCart, guestCartItemQuantity} = require('../../controller/buyer/loggedInCart')
+const {loggedInAddItem, loggedInUpdateItemQuantity, loggedInDeleteItem, loggedInIndexCart, loggedInCartItemQuantity} = require('../../controller/buyer/loggedInCart')
 
 router.post('/:id', passportAuthenticate, loggedInAddItem)
 
@@ -12,6 +12,6 @@ router.delete('/delete/:id', passportAuthenticate, loggedInDeleteItem)
 
 router.get('/cart', passportAuthenticate, loggedInIndexCart)
 
-router.get('/quantity/:id', passportAuthenticate, guestCartItemQuantity)
+router.get('/quantity/:id', passportAuthenticate, loggedInCartItemQuantity)
 
 module.exports = router
