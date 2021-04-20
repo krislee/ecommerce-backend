3a16561ec148b5d8a50df75c164fbac7e4346763
Kristy Lee <leekristy01@gmail.com>
2021-04-20 17:30:41 -0400

    testing where null and false comes from in passport.authenticate()

--- auth/passport.js
+++ auth/passport.js
@@ -18,8 +18,8 @@ const options = {
 
 // When making the passport jwt strategy (new JWTStrategy), the passport jwt strategy has already verified the JWT token by going through the options. So as indicated in the options it will grab the JWT token from the Auth HTTP header and grab the public key. The token and public key are passed into the verify function found in jsonwebtoken library. After verifying, the payload obj is passed into verify callback and the verify callback is called.
 const strategy = new JWTStrategy(options, async (payload, done) => { 
-    console.log(21, "TOKEN in strategy", payload.sub)
     try {
+        console.log(21, "TOKEN in strategy", payload.sub)
         const seller = await SellerUser.findById(payload.sub)
         // console.log(seller, 'seller from passport')
         if(await seller) return done(null, seller)
@@ -30,7 +30,7 @@ const strategy = new JWTStrategy(options, async (payload, done) => {
         
         console.log('random password')
 
-        return done(null, false) // if there were no errors from verifying JWT (i.e. correct signature and data not tampered) but no user is found from the payload
+        return done(27, 38) // if there were no errors from verifying JWT (i.e. correct signature and data not tampered) but no user is found from the payload
     } catch (err) {
         console.log('ERROR FROM PASSPORT STRATEGY\n----------------')
         console.log(36, err)
