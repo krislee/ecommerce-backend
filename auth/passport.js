2441389ebfd9df182d289974a796d935b0df65af
Kristy Lee <leekristy01@gmail.com>
2021-04-20 20:41:00 -0400

    error removed

--- auth/passport.js
+++ auth/passport.js
@@ -22,7 +22,6 @@ const options = {
 // When making the passport jwt strategy (new JWTStrategy), the passport jwt strategy has already verified the JWT token by going through the options. So as indicated in the options it will grab the JWT token from the Auth HTTP header and grab the public key. The token and public key are passed into the verify function found in jsonwebtoken library. After verifying, the payload obj is passed into verify callback and the verify callback is called.
 const strategy = new JWTStrategy(options, async (payload, done) => { 
     try {
-        if(err) console.log(23, 'ERROR FROM PASSPORT STRATEGY\n----------------\n', err)
         console.log(21, "TOKEN in strategy", payload.sub)
         const seller = await SellerUser.findById(payload.sub)
         // console.log(seller, 'seller from passport')
