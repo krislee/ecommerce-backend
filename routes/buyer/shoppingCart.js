9a245240503776130d73b56d3b7a00d7270e5f73
Kristy Lee <leekristy01@gmail.com>
2021-04-20 23:11:07 -0400

    splitting the JWT token

--- routes/buyer/shoppingCart.js
+++ routes/buyer/shoppingCart.js
@@ -48,13 +48,16 @@ router.get('/cartID', passportAuthenticate, getCartID)
 router.get('/cart-item/:id', (req, res) => {
     console.log(48, "AUTHORIZATION IN SHOPPING CART ROUTE", req.headers.authorization)
     const token = req.headers.authorization
+    token = token.split("Bearer ")
+    console.log(token)
+    console.log(token[1])
     // console.log(49, "AUTHORIZATION", req.headers)
     // console.log(50, "-----: ", req.body)
     // console.log(51, "req.body.auth: ", req.body.Authorization)
     if(!req.headers.authorization) {
         res.redirect(307, `/guest/buyer/quantity/${req.params.id}`)
     } else {
-        res.redirect(307, `/loginbuyer/quantity/${req.params.id}?token=${token}`)
+        res.redirect(307, `/loginbuyer/quantity/${req.params.id}?token=${token[1]}`)
     }
     
 })
